500bbdf8b0c43b614204fc94ca73da48060c278b
Thai Hoa Nguyen <tnguyen568@my.bcit.ca>
2025-02-20 19:39:42 -0800

    Changed and testing gitignore file

--- obj/Debug/net9.0/Assignment_1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Assignment_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+248c283137e0ce6e2a66d20c17f81019a5e8cd50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97a1985a9468f3a6c0ab245dec598404323dbcf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
